16f04826466e0902d7102fc2e2b20a178043021d
Cassandra King <puddincat@gmail.com>
2018-04-03 16:10:59 -0400

    add click to show coordinates on form map

--- app/javascript/components/MapTile.js
+++ app/javascript/components/MapTile.js
@@ -3,7 +3,13 @@ import { Route, IndexRoute, Router, browserHistory, hashHistory, Link } from 're
 import ReactMapboxGl, { Layer, Feature, ZoomControl, GeolocateControl } from "react-mapbox-gl";
 import { geoData } from '../Constants';
 const accessToken = "pk.eyJ1IjoiY2Fzc2tpbmciLCJhIjoiY2plcnRzaDJiMDAxYzJ2bnZ0OGU3dnB3OSJ9.kUHTVfObT_1gNrIdQM6eIQ"
-
+const routeCoordinates =[
+[-84.518399,39.134126],[-84.51841,39.133781],[-84.520091,39.133389],[-84.520497,
+39.131655],[-84.520852,39.128039],[-84.52036,39.127901],[-84.52094,39.122783],
+[-84.52022,39.122713],[-84.520768,39.120841],[-84.519639,39.120268],
+[-84.513743,39.115317],[-84.514554,39.114744],[-84.514307,39.114531],
+[-84.514551,39.114249],[-84.511692,39.102682],[-84.511987,39.102638]
+]
 const Map = ReactMapboxGl({
   accessToken: accessToken
   });
@@ -26,29 +32,29 @@ const layerStyles = {
   },
 };
 
-
 const MapTile = (props) => {
   return(
     <div className="mapTile">
       <Map
         accessToken={accessToken}
         style="mapbox://styles/cassking/cjfachvt46kps2sob2eqponl6"
-        zoom={props.zoomLevel}
+        zoomLevel={[16]}
         animated={props.animated}
         interactive='true'
+        userLocationVisible={props.userLocationVisible}
         showUserLocation={props.showUserLocation}
         containerStyle={{
           height: "90vh",
           width: "90vw"
         }}
 
-        center={props.center}
+        center={routeCoordinates[0]}
         >
         <Layer
           type="line"
           layout={lineLayout}
           paint={linePaint}>
-          <Feature coordinates={props.coordinates}/>
+          <Feature coordinates={routeCoordinates}/>
         </Layer>
 
     </Map>
--- app/javascript/containers/MapInFormContainer.js
+++ app/javascript/containers/MapInFormContainer.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactMapboxGl, {ScaleControl,ZoomControl,RotationControl,Layer,Feature } from "react-mapbox-gl";
 const accessToken = "pk.eyJ1IjoiY2Fzc2tpbmciLCJhIjoiY2plcnRzaDJiMDAxYzJ2bnZ0OGU3dnB3OSJ9.kUHTVfObT_1gNrIdQM6eIQ";
-// const style = "mapbox://styles/cassking/cjfj1kxtmf3uy2ro3ybukbc26";
 
-const style= "mapbox://styles/mapbox/streets-v10"
+const style= "mapbox://styles/mapbox/outdoors-v10"
 const Map = ReactMapboxGl({
   accessToken
 });
+
+let mymap = Map
 const containerStyle = {
   height: '70vh',
   width: '70vw'
 };
 const center = [-75.163685, 39.952345]
-const route: Route = require('./data/route.json');
 const lineLayout = {
   'line-cap': 'round',
   'line-join': 'round'
@@ -22,60 +22,34 @@ const linePaint = {
   'line-color': '#4790E5',
   'line-width': 12
 };
-const mappedRoute = route.points.map( point => [point.lat, point.lng]);
-const route2 = 'https://api.mapbox.com/directions/v5/mapbox/cycling/-84.518641,39.134270;-84.512023,39.102779?geometries=geojson&access_token=pk.eyJ1IjoiY2Fzc2tpbmciLCJhIjoiY2plcnRzaDJiMDAxYzJ2bnZ0OGU3dnB3OSJ9.kUHTVfObT_1gNrIdQM6eIQ'
-interface Point {
-  lng: number;
-  lat: number;
-}
-interface Route {
-  [key: string]: any;
-  points: Point[];
-}
+let map = Map
 class MapInFormContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      center: [-75.163685, 39.952345],//philly is default
-      zoomLevel:[15],
-      location:'',
-       mappedRoute:[]
+      center: center, // starting position
+      zoom: 9,
+      map_start_latitude:0,
+      map_start_longitude:0,
+      map_start_latitude:0,
+      map_end_latitude:0 // starting zoom
     }
-    this.onRouteChange = this.onRouteChange.bind(this)
-    this.getRoute=this.getRoute.bind(this)
-  }
-
-  onRouteChange(e){
-    this.setState({ mappedRoute: e.target.value });
-  }
-
-  componentDidMount() {
-    fetch('https://api.mapbox.com/directions/v5/mapbox/cycling/-84.518641,39.134270;-84.512023,39.102779?geometries=geojson&access_token=pk.eyJ1IjoiY2Fzc2tpbmciLCJhIjoiY2plcnRzaDJiMDAxYzJ2bnZ0OGU3dnB3OSJ9.kUHTVfObT_1gNrIdQM6eIQ')
-    .then(response => {
-      let parsed = response.json()
-      return parsed
-    }).then(route_data => {
-      console.log('route data ',route_data.routes[0].geometry.coordinates[0])
-      console.log('mapped', mappedRoute[0])
-
-      this.setState({
-        mappedRoute: route_data.routes[0].geometry.coordinates,
-        center:route_data.routes[0].geometry.coordinates[0]
-            })
-    })
-    alert(this.state.center)
+    this.getCoordinatesOnClick=this.getCoordinatesOnClick.bind(this)
   }
 
 
-getRoute() {
- // let start = [-84.518641, 39.134270];
- //  let end = [-84.512023, 39.102779];
-let routeRequest ='https://api.mapbox.com/directions/v5/mapbox/cycling/-84.518641,39.134270;-84.512023,39.102779?geometries=geojson&access_token=pk.eyJ1IjoiY2Fzc2tpbmciLCJhIjoiY2plcnRzaDJiMDAxYzJ2bnZ0OGU3dnB3OSJ9.kUHTVfObT_1gNrIdQM6eIQ'
+getCoordinatesOnClick(map, e){
+  document.getElementById('info').innerHTML =
+  JSON.stringify(e.lngLat);
 }
+
 
   render() {
     return (
+      <div><pre id='info'></pre>
       <Map
+        onClick={this.getCoordinatesOnClick}
+        // onMouseOver={this.getCoordinatesOnClick}
         zoomLevel={this.state.zoomLevel}
         style={style}
         showsUserLocation={true}
@@ -86,6 +60,7 @@ let routeRequest ='https://api.mapbox.com/directions/v5/mapbox/cycling/-84.51864
         <Feature coordinates={this.state.mappedRoute}  />
       </Layer>
 </Map>
+</div>
 
     );
   }
